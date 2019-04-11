b0c01ceed0373c422d6673bb3cca6bc94e76b273
polskiprogramistanet <rav@polskiprogramista.net>
2019-04-11 14:24:10 +0200

    Tester

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -11,11 +11,22 @@ namespace Tester
     {
         static void Main(string[] args)
         {
-
-            Status state = new Status();
-
-
+            /*
+             Aplikacja testująca bibliotekę dekodującą status sterownika Petromis 3000
+             Sterownik przesyła dwa bajty statusu (bajt S1 i bajt S0) do biblioteki libStatus
+             i otrzymujemy informację w postaci enumatora o faktycznym stanie urządzenia.
+             spodziewane wartości:
+             - S0=30 S1=31 : Gotowy
+             - S0=30 S1=32 : Zablokowany
+             - S0=30 S1=30 : Tankuje
+             W przykładzie wykorzystałem dwa wzorce 
+             - chain of Responsiblity - w celu porównania wartości wchodzących i podjęcia decyzji
+             - observer (obserwator) do uaktualnienia statusu na podstawie przekazanych bajtów
+             */
+            Status state = new Status();    //Inicjalizacja biblioteki
             Console.WriteLine(state.GetStatus(30, 31));
+            Console.WriteLine(state.GetStatus(30, 30));
+            Console.WriteLine(state.GetStatus(30, 32));
 
 
             Console.ReadKey();
